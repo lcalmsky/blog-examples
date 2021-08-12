82a185f608f6b3ab10a070525bf6ad97ff06360f
Jaime <lcalmsky@gmail.com>
2021-08-13 00:43:51 +0900

    Initial commit

---
+++ src/main/java/io/lcalmsky/blogexamples/BlogExamplesApplication.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.blogexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lcalmsky/blogexamples/BlogExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.blogexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
